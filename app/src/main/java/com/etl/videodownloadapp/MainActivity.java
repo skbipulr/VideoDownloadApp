152f3f57c15087360e430f8dcf224a57671f22db
Bipul Sarkar <skbipulr@gmail.com>
2020-01-07 23:53:00 +0600

    add progressbar and remove botton

--- app/src/main/java/com/etl/videodownloadapp/MainActivity.java
+++ app/src/main/java/com/etl/videodownloadapp/MainActivity.java
@@ -1,104 +1,191 @@
 package com.etl.videodownloadapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.PortUnreachableException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+
     private VideoView videoView;
-    private Button btn_download;
+    private static final String DOWNLOAD_URL = "http://androhub.com/demo/demo.mp4";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        btn_download = (Button) findViewById(R.id.download_btn);
-        videoView=(VideoView)findViewById(R.id.videoView);
-        Uri vidUrl=Uri.parse("http://androhub.com/demo/demo.mp4");
-        videoView.setVideoURI(vidUrl);
-        videoView.start();
-        btn_download.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        videoView = (VideoView) findViewById(R.id.videoView);
 
-                download();
-                Toast.makeText(MainActivity.this, "Click", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-    }
-
-    private void download() {
-        DownBack DB = new DownBack();
-        DB.execute("");
+        if (isPermissionGranted())
+            new DownloadFile().execute(DOWNLOAD_URL);
+        else
+            requestPermission();
 
     }
 
 
-    private class DownBack extends AsyncTask<String, String, String> {
+
+    private boolean isPermissionGranted() {
+        return ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+    }
+
+    // DownloadFile AsyncTask
+    private class DownloadFile extends AsyncTask<String, Integer, String> {
+        ProgressDialog mProgressDialog;
+        private final int MAX_INT = 100;
+        private final String FILE_NAME = "demo.mp4";
+        String filepath = Environment.getExternalStorageDirectory().getPath();
+
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            mProgressDialog = new ProgressDialog(MainActivity.this);
+            mProgressDialog.setTitle("Downloading.....");
+        //    mProgressDialog.setMessage("Please Wait!");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.setMax(MAX_INT);
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            mProgressDialog.show();
         }
 
         @Override
-        protected String doInBackground(String... strings) {
-            final String vidurl = "http://androhub.com/demo/demo.mp4";
-            downloadFile(vidurl);
-            return null;
+        protected String doInBackground(String... Url) {
+            try {
+                URL url = new URL(Url[0]);
+                URLConnection connection = url.openConnection();
+                connection.connect();
 
-        }
+                int fileLength = connection.getContentLength();
 
+                InputStream input = new BufferedInputStream(url.openStream());
 
-    }
+                OutputStream output = new FileOutputStream(filepath + "/" + FILE_NAME);
 
-    private void downloadFile(String vidUrl) {
+                byte data[] = new byte[1024];
+                long total = 0;
+                int count;
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    // Publish the progress
+                    publishProgress((int) (total * 100 / fileLength));
+                    output.write(data, 0, count);
+                }
 
-        SimpleDateFormat sd = new SimpleDateFormat("yymmhh");
-        String date = sd.format(new Date());
-        String name = "video" + date + ".mp4";
-
-        try {
-            String rootDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-                    + File.separator + "My_Video";
-            File rootFile = new File(rootDir);
-            rootFile.mkdir();
-            URL url = new URL(vidUrl);
-            HttpURLConnection c = (HttpURLConnection) url.openConnection();
-            c.setRequestMethod("GET");
-            c.setDoOutput(true);
-            c.connect();
-            FileOutputStream f = new FileOutputStream(new File(rootFile,
-                    name));
-            InputStream in = c.getInputStream();
-            byte[] buffer = new byte[1024];
-            int len1 = 0;
-            while ((len1 = in.read(buffer)) > 0) {
-                f.write(buffer, 0, len1);
+                // Close connection
+                output.flush();
+                output.close();
+                input.close();
+            } catch (Exception e) {
+                // Error Log
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
             }
-            f.close();
-        } catch (IOException e) {
-            Log.d("Error....", e.toString());
+            return null;
         }
 
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            super.onProgressUpdate(progress);
+            mProgressDialog.setProgress(progress[0]);
+
+            Log.e("Progress", progress[0].toString());
+            if (progress[0] == MAX_INT) {
+                videoView.setVideoURI(Uri.parse(DOWNLOAD_URL));
+                videoView.start();
+                mProgressDialog.dismiss();
+                videoView.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 2 && isPermissionGranted()) {
+            new DownloadFile().execute(DOWNLOAD_URL);
+        } else {
+
+            boolean showRationale = shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            if (!showRationale) {
+                // user also CHECKED "never ask again"
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Permission Not Granted")
+                        .setMessage("Grant Permissions from settings")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        })
+                        .show();
+            } else {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Permission Not Granted")
+                        .setMessage("Grant Permission to continue")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                requestPermission();
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                                Toast.makeText(getApplicationContext(), "Closing App", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
+            }
+        }
+    }
+
+
 }
